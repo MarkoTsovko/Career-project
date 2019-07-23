8612fd40ebc40dbe5c5fd220b6c4fe84a958e576
MarkoTsovko <53077771+MarkoTsovko@users.noreply.github.com>
2019-07-23 23:49:40 +0300

    First page index.html
    Without responsive nav bar. < 575 px.

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$('.partners__slider').slick({
+		arrows: true,
+		autoplay: true,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+/*      	prevArrow: $('.next'),
+      	nextArrow: $('.prev'),*/
+      	  responsive: [
+    {
+      breakpoint: 1199,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+	});
+});
